1912455755c00ca6e105766ffd705cf6cf98f0f1
guibe22 <galvezwilber22@gmail.com>
2023-04-14 19:02:18 -0400

    first commit

---
+++ HolaMundo/app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.holamundo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.holamundo.ui.theme.HolaMundoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HolaMundoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Text(text = "probando mi primera app")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HolaMundoTheme {
+        Greeting()
+    }
+}
